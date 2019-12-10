dcd6dec5e8ba4b96e3db793e9f3ca420a2830806
Shanmukh Koka <46795444+saikoka@users.noreply.github.com>
2019-12-10 14:50:09 -0500

    Add files via upload

--- colorizer_2.py
+++ colorizer_2.py
@@ -19,7 +19,20 @@ def save_grayscale(rgb):
     index= rgb.index(".jpg")
     cv2.imwrite(rgb[:index]+"_bw"+rgb[index:], img_grayscale)
 
-for entry in os.scandir():
+"""for entry in os.scandir():
     if entry.name.endswith(".jpg") and "_bw" not in entry.name and entry.is_file():
-        save_grayscale(entry.name)
+        save_grayscale(entry.name)"""
+
+#Scales rgb value for black/white image
+img_bw=cv2.imread('image_0001_bw.jpg',1)
+new_img_bw_scaled=img_bw/255.0
+plt.imshow(new_img_bw_scaled)
+plt.show()
+
+#scales rgb values for colored image. 
+img =cv2.imread('image_0001.jpg',1)
+new_img_scaled=img/255.0
+plt.imshow(new_img_scaled)
+plt.show()
+
 
